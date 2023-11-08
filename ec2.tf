096469d77d58d118702002f71e6468566bc22d21
Jake Siney <jakesiney@gmail.com>
2023-11-08 16:08:08 +0000

    Changed SG

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-projects-server" {
   ami             = "ami-06ce6bb40e50efe77"
   instance_type   = "t2.micro"
   key_name        = "uploader-key"
-  security_groups = ["terraform-projects-server"]
+  security_groups = "sg-0d395c7eaffbaaf93"
   tags = {
     Name = "terraform-projects-server"
   }
